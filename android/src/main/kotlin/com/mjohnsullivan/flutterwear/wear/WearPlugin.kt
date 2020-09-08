ac021d3a13c0a0d8be4dbad58c95bdd43c90022d
Simon Lightfoot <simon@devangels.london>
2020-09-08 21:54:40 +0100

    Updated to AndroidX and Android embedding v2.

--- android/src/main/kotlin/com/mjohnsullivan/flutterwear/wear/WearPlugin.kt
+++ android/src/main/kotlin/com/mjohnsullivan/flutterwear/wear/WearPlugin.kt
@@ -1,69 +1,85 @@
 package com.mjohnsullivan.flutterwear.wear
 
+import android.os.Bundle
+import androidx.core.view.ViewCompat.requestApplyInsets
+import androidx.core.view.ViewCompat.setOnApplyWindowInsetsListener
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.FragmentActivity
+import androidx.wear.ambient.AmbientModeSupport
+import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
+import io.flutter.plugin.common.BinaryMessenger
+import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.MethodCall
-import io.flutter.plugin.common.PluginRegistry.Registrar
-import io.flutter.view.FlutterView
-
-import android.app.Activity
-import android.os.Bundle
-
-import android.support.v4.view.ViewCompat.requestApplyInsets
-import android.support.v4.view.ViewCompat.setOnApplyWindowInsetsListener
-import android.support.v4.view.WindowInsetsCompat
-
-import android.support.wear.ambient.AmbientMode
 
 private const val CHANNEL_NAME = "wear"
 
-fun getChannel(view: FlutterView): MethodChannel {
-    return MethodChannel(view, CHANNEL_NAME)
+fun getChannel(messenger: BinaryMessenger?): MethodChannel {
+    return MethodChannel(messenger, CHANNEL_NAME)
 }
 
-class WearPlugin private constructor(
-        activity: Activity,
-        private val view: FlutterView,
-        val channel: MethodChannel) :
-        MethodCallHandler {
+class WearPlugin : FlutterPlugin, ActivityAware, MethodCallHandler {
 
-    private var mAmbientController: AmbientMode.AmbientController? = null
+    private var mActivityBinding: ActivityPluginBinding? = null
+    private var mMethodChannel: MethodChannel? = null
+    private var mAmbientController: AmbientModeSupport.AmbientController? = null
 
-    init {
-        // Set the Flutter ambient callbacks
-        mAmbientController = AmbientMode.attachAmbientSupport(activity)
+    override fun onAttachedToEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+        mMethodChannel = MethodChannel(binding.binaryMessenger, CHANNEL_NAME)
+        mMethodChannel?.setMethodCallHandler(this)
     }
 
-    companion object {
-        @JvmStatic
-        fun registerWith(registrar: Registrar) {
-            val channel = MethodChannel(registrar.messenger(), CHANNEL_NAME)
-            channel.setMethodCallHandler(WearPlugin(registrar.activity(),
-                    registrar.view(), channel))
-        }
+    override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+        mMethodChannel?.setMethodCallHandler(this)
+        mMethodChannel = null
     }
 
+    override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+        assert(mMethodChannel != null)
+        mActivityBinding = binding
+        mAmbientController = AmbientModeSupport.attach(binding.activity as FragmentActivity)
+    }
 
+    override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+        assert(mMethodChannel != null)
+        mActivityBinding = binding
+        mAmbientController = AmbientModeSupport.attach(binding.activity as FragmentActivity)
+    }
+
+    override fun onDetachedFromActivityForConfigChanges() {
+        mMethodChannel?.setMethodCallHandler(null)
+        mActivityBinding = null
+    }
+
+    override fun onDetachedFromActivity() {
+        mMethodChannel?.setMethodCallHandler(null)
+        mActivityBinding = null
+    }
 
     override fun onMethodCall(call: MethodCall, result: Result) {
-        when {
-            call.method == "getPlatformVersion" ->
-                result.success("Android ${android.os.Build.VERSION.RELEASE}")
-            call.method == "getShape" -> handleShapeMethodCall(result)
+        when (call.method) {
+            "getShape" -> handleShapeMethodCall(result)
             else -> result.notImplemented()
         }
     }
 
     private fun handleShapeMethodCall(result: Result) {
-        setOnApplyWindowInsetsListener(view, { _, insets: WindowInsetsCompat? ->
+        val view = mActivityBinding?.activity?.window?.decorView
+        if (view == null) {
+            result.error("No View", null, null)
+            return
+        }
+        setOnApplyWindowInsetsListener(view) { _, insets: WindowInsetsCompat? ->
             if (insets?.isRound == true) {
                 result.success(0)
             } else {
                 result.success(1)
             }
             WindowInsetsCompat(insets)
-        })
+        }
         requestApplyInsets(view)
     }
 }
@@ -71,7 +87,7 @@ class WearPlugin private constructor(
 /*
  * Pass ambient callback back to Flutter
  */
-class FlutterAmbientCallback(private val channel: MethodChannel) : AmbientMode.AmbientCallback() {
+class FlutterAmbientCallback(private val channel: MethodChannel) : AmbientModeSupport.AmbientCallback() {
 
     override fun onEnterAmbient(ambientDetails: Bundle) {
         channel.invokeMethod("enter", null)
